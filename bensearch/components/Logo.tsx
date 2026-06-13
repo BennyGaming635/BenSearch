f44b0e29af371b98be29fbdcc1503ee2974f0100
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-13 18:58:35 +0930

    center text

--- bensearch/components/Logo.tsx
+++ bensearch/components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
     return (
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-5xl font-bold text-center">
             BenSearch
         </h1>
     );
