435ed78589004abcbf6902a9f9bded9363d3610c
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-13 18:23:03 +0930

    add component for search results

--- bensearch/components/SearchResult.tsx
+++ bensearch/components/SearchResult.tsx
@@ -3,3 +3,25 @@ interface ResultProps {
     url: string;
     description: string;
 }
+
+export default function SearchResult({
+    title,
+    url,
+    description
+}: ResultProps) {
+    return (
+        <a
+            href={url}
+            target="_blank"
+            className="block p-4 rounded-lg border"
+        >
+            <h2 className="text-xl font-semibold">
+                {title}
+            </h2>
+            <p>{description}</p>
+            <span className="text-black/50">
+                {url}
+            </span>
+        </a>
+    );
+}
