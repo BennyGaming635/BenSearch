0b61ced8a87964550bfc6019fef3e803dd9d2510
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-13 19:14:10 +0930

    add link to home and change coding to code

--- bensearch/components/Logo.tsx
+++ bensearch/components/Logo.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 export default function Logo() {
     return (
         <h1 className="text-5xl font-bold text-center">
-            BenSearch
+            <Link href="/">BenSearch</Link>
         </h1>
     );
 }
