d1132b57998ec33ead61c7503cb2696be998093e
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-15 20:18:40 +0930

    use noresults on search page

--- bensearch/app/search/page.tsx
+++ bensearch/app/search/page.tsx
@@ -75,14 +75,7 @@ export default async function Search({
             </div>
 
             {results.length === 0 && (
-                <div className="text-center mt-12">
-                    <h2 className="text-xl font-semibold">
-                        No results found.
-                    </h2>
-                    <p className="text-gray-500 mt-2">
-                        Try searching for something else?
-                    </p>
-                </div>
+                <NoResults query={rawQuery} />
             )}
         </main>
     );
