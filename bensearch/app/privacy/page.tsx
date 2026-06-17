de724a4a5ffd2a009bafe17f051c0ae1bf681ae8
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2026-06-17 19:24:52 +0930

    add heading

---
+++ bensearch/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+export default function Privacy() {
+    return (
+        <main className="min-h-screen px-6 py-16 max-w-3xl mx-auto">
+            <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
+        </main>
+    )
+}
